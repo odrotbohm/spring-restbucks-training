408e379440a39ea8a475d018890e7f0dda222457
Oliver Gierke <info@olivergierke.de>
2013-09-10 10:27:32 -0700

    Removed Spring Data REST config where not needed anymore.

--- rest-2/src/main/java/org/springsource/restbucks/training/order/OrderRepository.java
+++ rest-2/src/main/java/org/springsource/restbucks/training/order/OrderRepository.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.repository.annotation.RestResource;
 import org.springsource.restbucks.training.order.Order.Status;
 
 /**
@@ -27,7 +26,6 @@ import org.springsource.restbucks.training.order.Order.Status;
  * 
  * @author Oliver Gierke
  */
-@RestResource(path = "orders", rel = "orders")
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
 	/**
